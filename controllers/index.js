5cdfd22a957765618a71d39da010abc50f763108
DerizhyLouise <louise.derizhy@gmail.com>
2023-10-03 12:44:07 +0700

    API order

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const userController = require('./userController');
 const customerController = require('./customerController');
 const productController = require('./productController');
+const orderController = require('./orderController');
 
 module.exports = {
     userController,
     customerController,
-    productController
+    productController,
+    orderController
 };
