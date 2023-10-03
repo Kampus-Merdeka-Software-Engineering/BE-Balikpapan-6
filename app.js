aa49d8c545ebbeb3e810e37946a20820dbd1e8f1
DerizhyLouise <louise.derizhy@gmail.com>
2023-10-03 23:58:34 +0700

    tes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const { logger } = require('./middleware/logger');
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const routes = require('./routes');
 
